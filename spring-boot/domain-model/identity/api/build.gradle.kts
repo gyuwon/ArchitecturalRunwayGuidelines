a387f74076f64690069643fafc8a7e2cd3db7a4f
Gyuwon Yi <gyuwon@live.com>
2024-10-08 13:47:00 +0900

    불필요한 의존성 제거

--- spring-boot/domain-model/identity/api/build.gradle.kts
+++ spring-boot/domain-model/identity/api/build.gradle.kts
@@ -19,6 +19,4 @@ dependencies {
     implementation(project(":domain-model:identity:adapters"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
